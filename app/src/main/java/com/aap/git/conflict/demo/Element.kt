d75cd30aca740eb3829b0203b93badf538b18cb4
Ashutosh Pendse <apendse@gmail.com>
2023-04-26 12:59:25 -0700

    Added solids

--- app/src/main/java/com/aap/git/conflict/demo/Element.kt
+++ app/src/main/java/com/aap/git/conflict/demo/Element.kt
@@ -3,6 +3,10 @@ package com.aap.git.conflict.demo
 enum class Element(val symbol: String, val fullName: String, val atomicNumber: Int) {
     HYDROGEN("H", "Hydrogen", 1),
     HELIUM("He", "Helium", 2),
+    LITHIUM("Li", "Lithium", 3),
+    BERYLLIUM("Be", "Beryllium", 4),
+    BORON("B", "Boron", 5),
+    CARBON("C", "Carbon", 6),
     NITROGEN("N", "Nitrogen", 7),
     OXYGEN("O", "Oxygen", 8),
     Fluorine("F", "Fluorine", 9),
